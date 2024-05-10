bdf1b304c84d83ed76b2e08e0dd65dca64dfe9ae
kingkhado <144692407+kingkhado@users.noreply.github.com>
2024-05-10 19:57:01 +0200

    fixed greety thingy

--- Scripts/GreetThingy.js
+++ Scripts/GreetThingy.js
@@ -2,5 +2,5 @@ function displayGreeting()
 {
     var name = document.getElementById("nameInput").value;
     var greeting = document.getElementById("greeting");
-    greeting.textContent = "Heyi wena " + name + ", this is my website get outta here no! :D , jokes . Welcome to the party";
+    greeting.textContent = "Heyi wena " + name + ", this is my website get outta here now! :D , jokes . Welcome to the party with Sir Khadi!";
 }
